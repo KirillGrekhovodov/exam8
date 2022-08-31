e28f8e96596bb92dfd2ac7dc4defcb1d424b5d4a
”Kirill <kirillgrekhovodov@gmail.com>
2022-08-31 16:36:54 +0600

    Создал модели продукта и отзыва, добавил приложение accounts

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+CHOISES = [('other', 'Разное'), ('phones', 'Телефоны'), ('headphones', 'Наушники'), ('cases', 'Чехол')]
+
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+    category = models.CharField(max_length=20, default='other', choices=CHOISES, verbose_name='Категории')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    picture = models.ImageField(verbose_name='Картинка', upload_to='pictures', null=True, blank=True)
+
+    class Meta:
+        db_table = "products"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return f"{self.name} - {self.category}"
+
+
+class Review(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='reviews', verbose_name='Автор')
+    product = models.ForeignKey('webapp.Product', on_delete=models.CASCADE, related_name='reviews',
+                                verbose_name='Товар')
+    text = models.TextField(max_length=2000, verbose_name='Текст отзыва')
+    mark = models.IntegerField(verbose_name='Оценка', validators=[MaxValueValidator(5), MinValueValidator(1)])
+    is_moderated = models.BooleanField(verbose_name='Подтвержден', default=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    edited_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
+
+    class Meta:
+        db_table = "reviews"
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
+    def __str__(self):
+        return f"{self.author.username} - {self.product.name}"
--- webinar23/settings.py
+++ webinar23/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -117,6 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = "accounts:login"
+LOGOUT_REDIRECT_URL = "webapp:index"
+LOGIN_REDIRECT_URL = "webapp:index"
+
+MEDIA_ROOT = Path.joinpath(BASE_DIR, "uploads")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
